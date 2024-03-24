ec142a00991d7285bc77c97787d38f98250c44ca
Gabriel Fronda <gabriel.fronda.cics@ust.edu.ph>
2024-03-24 11:00:57 +0800

    ayos welcome user

--- header.php
+++ header.php
@@ -27,8 +27,8 @@
         </a>
 
         <?php if (!empty($username)) { ?>
-            <h2 style='color: #fff; font-weight: bold; margin-right:500px;' >Welcome!
-                <?php echo $username; ?>
+            <h2 style='color: #fff; font-weight: bold; margin-right:500px;' >Welcome,
+                <?php echo $username; ?>!
             </h2>
             <ul class="links">
                 <span class="close-btn material-symbols-rounded">close</span>
