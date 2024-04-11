306eb223d7396d1fc8fae6c32ba6d279666ff5f0
Gabriel Fronda <gabriel.fronda.cics@ust.edu.ph>
2024-04-12 00:41:45 +0800

    FINAL

--- terms.php
+++ terms.php
@@ -256,13 +256,7 @@ function goBack() {
 				</svg>
 				Terms & Conditions
 			</h1>
-			<button class="icon-button">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-					<path fill="none" d="M0 0h24v24H0z" />
-                    <a href="content.php">
-					<path fill="currentColor" d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
-				</svg>
-			</button>
+			
 		</header>
 		<section class="modal-container-body rtf">
 			<h2>Payment (Downpayment)</h2>
